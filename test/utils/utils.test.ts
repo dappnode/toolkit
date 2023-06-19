dbbf0da570e1dfe76663326245896993e491e4aa
Diego <dsimog01@estudiantes.unileon.es>
2023-06-19 11:27:29 +0200

    Remove isEnsDomain tests (already in types)

--- test/utils/utils.test.ts
+++ test/utils/utils.test.ts
@@ -1,39 +1,7 @@
 import { expect } from "chai";
-import { isEnsDomain, ensureValidDnpName } from "../../src/utils.js";
+import { ensureValidDnpName } from "../../src/utils.js";
 
 describe("Util functions", function () {
-  describe("isEnsDomain", function () {
-    it("should return true for a valid ENS domain", async () => {
-      const validEnsDomains = [
-        "example.eth",
-        "subdomain.example.eth",
-        "sub-domain.example.eth",
-        "beacon-chain.prysm.dnp.dappnode.eth",
-      ];
-
-      for (const domain of validEnsDomains) {
-        expect(isEnsDomain(domain)).to.be.true;
-      }
-    });
-
-    it("should return false for an invalid ENS domain", async () => {
-      const invalidEnsDomains = [
-        "example.com",
-        "subdomain.example.com",
-        "sub-domain.example.com",
-        "dnp.dappnode.com",
-        "example",
-        "",
-        "example/.dnp.eth",
-        "..dappnode.eth",
-      ];
-
-      for (const domain of invalidEnsDomains) {
-        expect(isEnsDomain(domain)).to.be.false;
-      }
-    });
-  });
-
   describe("ensureValidDnpName", function () {
     it("should return the valid DNP name", async () => {
       const validDnpNames = [
